31317456356be0761a52e0d9056e7035f8f6e104
Aniket Singh <aniketsingh6990@gmail.com>
2023-02-14 12:25:50 +0530

    account and balance fetch init

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
+import { ethers } from "ethers";
 import * as vscode from "vscode";
+import { shortenAddress } from "./utils";
 
 interface NativeCurrency {
   name: string;
@@ -13,18 +15,33 @@ interface NetworkConfig {
   nativeCurrency: NativeCurrency;
 }
 
-let myStatusBarItem: vscode.StatusBarItem;
+let myStatusBarItemPrimary: vscode.StatusBarItem;
+let myStatusBarItemSecondary: vscode.StatusBarItem;
 const extension = vscode.extensions.getExtension("7finney.ethcode");
 const ethcode = extension?.exports;
 
 export async function activate({ subscriptions }: vscode.ExtensionContext) {
   // create a new status bar item that we can now manage
 
-  myStatusBarItem = vscode.window.createStatusBarItem(
+  /**
+   * Status bar item for primary side
+   * structure: `Ethereum: <block number> <gas> is the primary item.`
+   */
+  myStatusBarItemPrimary = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    1
+  );
+  subscriptions.push(myStatusBarItemPrimary);
+
+  /**
+   * Status bar item for secondary side
+   * structure: `<account> <balance>> is the primary item.`
+   */
+  myStatusBarItemSecondary = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     100
   );
-  subscriptions.push(myStatusBarItem);
+  subscriptions.push(myStatusBarItemSecondary);
 
   /**
    *  register a command that is invoked when  statusbar item
@@ -32,11 +49,18 @@ export async function activate({ subscriptions }: vscode.ExtensionContext) {
    */
 
   const blockScannerCommand = "blockNumber.showBlockScanner";
+  const showAccountCommand = "blockNumber.showAccount";
+  vscode.commands.registerCommand(blockScannerCommand, () =>
+    vscode.commands.executeCommand(
+      "vscode.open",
+      vscode.Uri.parse(`${selectedNetworkConfig.blockScanner}`)
+    )
+  );
+
   subscriptions.push(
-    vscode.commands.registerCommand(blockScannerCommand, () => {
-      vscode.window.showInformationMessage(
-        `${selectedNetworkConfig.blockScanner}`
-      );
+    vscode.commands.registerCommand(showAccountCommand, async () => {
+      const accounts = await ethcode.wallet.list();
+      vscode.window.showInformationMessage(`${accounts[0]}`);
     })
   );
 
@@ -49,11 +73,35 @@ export async function activate({ subscriptions }: vscode.ExtensionContext) {
   const ethcodeProvider = ethcode.provider.get();
   ethcodeProvider.on("block", async (blockNumber: number) => {
     if (blockNumber > 0) {
-      myStatusBarItem.text = `$(symbol-constructor) ${blockNumber} ${selectedNetworkConfig.nativeCurrency.name}`;
-      myStatusBarItem.command = blockScannerCommand;
-      myStatusBarItem.show();
+      const gasPrice = await ethcodeProvider.getGasPrice();
+      const gasPriceInGwei = ethers.utils.formatUnits(gasPrice, "gwei");
+      myStatusBarItemPrimary.text = `$(symbol-constructor) ${selectedNetworkConfig.nativeCurrency.name}: ${blockNumber} ${gasPriceInGwei}`;
+      myStatusBarItemPrimary.command = blockScannerCommand;
+      myStatusBarItemPrimary.show();
     } else {
-      myStatusBarItem.hide();
+      myStatusBarItemPrimary.hide();
     }
   });
+  checkBalance(showAccountCommand, selectedNetworkConfig);
 }
+const checkBalance = async (command: string, networkConfig: NetworkConfig) => {
+  const ethcodeProvider = ethcode.provider.get();
+  const accounts: Array<string> = await ethcode.wallet.list();
+  if (accounts.length > 0) {
+    const balance = await ethcodeProvider
+      .getBalance(accounts[0])
+      .then((balance: number) => {
+        const balanceInEther = ethers.utils.formatEther(balance);
+        return balanceInEther;
+      });
+    const dataToPrint = `$(file-code) ${balance} ${
+      networkConfig.nativeCurrency.symbol
+    } ${shortenAddress(accounts[0], 4)}`;
+    myStatusBarItemSecondary.text = dataToPrint;
+    myStatusBarItemSecondary.command = command;
+    myStatusBarItemSecondary.show();
+  } else {
+    myStatusBarItemPrimary.hide();
+    vscode.window.showInformationMessage("No account available in ETHcode.");
+  }
+};
---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import { isAddress } from "@ethersproject/address";
+
+/**
+ * Shortens an Ethereum address by N characters
+ * @param address blockchain address
+ * @param charsStart amount of character to shorten (from both ends / in the beginning)
+ * @param charsEnd amount of characters to shorten in the end
+ * @returns formatted string
+ */
+export function shortenAddress(
+  address: string,
+  charsStart = 4,
+  charsEnd?: number
+): string {
+  return `${address.slice(0, charsStart + 2)}...${address.slice(
+    42 - (charsEnd || charsStart)
+  )}`;
+}
